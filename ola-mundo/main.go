f3f2b3d5b4081b67c89408c39c50feded6ea4f16
Wanderson Alves <wanderson.alves.rodrigues@gmail.com>
2025-08-24 13:39:08 -0300

    feat: crição do ola mundo

---
+++ ola-mundo/main.go
@@ -0,0 +1,10 @@
+// Todo arquivo Go começa com a declaração do pacote.
+package main
+
+// A cláusula import lista os pacotes que nosso programa usa.
+import "fmt"
+
+// A função main é onde a execução do programa começa.
+func main() {
+    fmt.Println("Olá, Mundo!")
+}
